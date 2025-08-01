70f1f7d93664f4f924cdc53faef65550f886675b
sakshiee34 <sakshi.mali@klebcaathani.com>
2025-08-01 15:15:51 +0530

    uploaded projects

--- router/jobapplicationroutes.js
+++ router/jobapplicationroutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 
 const router = express.Router();
-import { applyjob, deleteapplication, getapplicationsoflogineduser, updateapplication } from '../controller/jobapplicationcontroller.js';
+import { applyjob, deleteapplication, getapplicationsoflogineduser, getapplicationofparticularjob, updateapplication } from '../controller/jobapplicationcontroller.js';
 
 
 router.post('/apply/:jobid/:userid', applyjob);
 router.get('/apply/:userid', getapplicationsoflogineduser);
+router.get('/applicationsbyjobid/:jobid', getapplicationofparticularjob);
 router.put('/updateapplication/:id', updateapplication);
 router.delete('/deleteapplication/:id', deleteapplication)
 
